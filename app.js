5087e92f33cbe2be23a2c079d6f40dfbb773f9b8
Michael Herman <hermanmu@gmail.com>
2013-11-19 16:01:49 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 // dependencies
 var fs = require('fs');
+var http = require('http');
 var express = require('express');
-var routes = require('./routes');
 var path = require('path');
-var app = express();
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-// config
+// main config
+var app = express();
 app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// env config
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
 
 // mongo config
 var MONGOLAB_URI= "add_your_mongolab_uri_here"
@@ -24,6 +39,12 @@ mongoose.connect(mongo);
 // mongo model
 var Customer = require('./models.js');
 
+// passport config
+// var Account = require('./models/account');
+// passport.use(new LocalStrategy(Account.authenticate()));
+// passport.serializeUser(Account.serializeUser());
+// passport.deserializeUser(Account.deserializeUser());
+
 // parse objects through POST
 app.use(express.bodyParser());
 
@@ -31,6 +52,7 @@ app.use(express.bodyParser());
 require('./charge.js')(app);
 
 // routes
+var routes = require('./routes');
 app.get('/', routes.index);
 app.get('/ping', routes.ping);
 app.get('/charge', routes.charge);
@@ -39,5 +61,5 @@ app.get('/admin', routes.admin);
 
 // run server
 app.listen(app.get('port'), function(){
-  console.log('\nListening on port ' + app.get('port'));
+  console.log('\nExpress server listening on port ' + app.get('port'));
 });
