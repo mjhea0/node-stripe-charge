bf5f69842dbca85f12863f9fa6836809ba8dfdbb
Michael Herman <hermanmu@gmail.com>
2014-01-11 19:17:12 -0700

    test

--- test/index.js
+++ test/index.js
@@ -2,21 +2,26 @@ var app = require('../app');
 var request = require('supertest');
 
 describe('node-stripe-charge', function(){
-  describe('when requesting resource /ping', function(){
+  describe('when requesting resource /', function(){
     it('should respond with 200', function(done){
       request(app)
-        .get('/ping')
+        .get('/')
+        .expect('Content-Type', /html/)
         .expect(200, done);
     });
   });
 
-  describe('when requesting resource /', function(){
+  describe('when requesting resource /ping', function(){
     it('should respond with 200', function(done){
       request(app)
         .get('/ping')
+        .expect('Content-Type', /html/)
+        // .expect('text', 'pong!')
         .expect(200, done);
     });
   });
+
+
 
   // describe('when requesting resource /missing', function(){
   //   it('should respond with 404', function(done){
