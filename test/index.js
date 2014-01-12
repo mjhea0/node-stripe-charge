a0f753eaf87582e111885613cdc0e91991263a25
Michael Herman <hermanmu@gmail.com>
2014-01-11 18:11:33 -0700

    testing

--- test/index.js
+++ test/index.js
@@ -1,8 +1,16 @@
 var app = require('../app');
 var request = require('supertest');
 
-describe('vision', function(){
+describe('node-stripe-charge', function(){
   describe('when requesting resource /ping', function(){
+    it('should respond with 200', function(done){
+      request(app)
+        .get('/ping')
+        .expect(200, done);
+    });
+  });
+
+  describe('when requesting resource /', function(){
     it('should respond with 200', function(done){
       request(app)
         .get('/ping')
