9202c69e7494436ab2ffe53fceb0c5d6ed780094
Michael Herman <hermanmu@gmail.com>
2015-04-27 03:54:58 -0600

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
+// gulp
+
+var gulp = require('gulp');
+
+// plugins
+
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     jshint = require('gulp-jshint'),
     mocha = require('gulp-mocha');
 
-gulp.task('lint', function () {
-  gulp.src('./server/**/*.js')
-    .pipe(jshint());
+
+// tasks
+
+gulp.task('lint', function() {
+  gulp.src(['./server/**/*.js', './public/js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', function () {
@@ -19,7 +30,10 @@ gulp.task('test', function () {
     });
 });
 
-gulp.task('default', function () {
+
+// default task
+
+gulp.task('default', ['lint'], function () {
   nodemon({ script: './server/bin/www', ext: 'html js', ignore: ['ignored.js'] })
     .on('change', ['lint'])
     .on('restart', function () {
