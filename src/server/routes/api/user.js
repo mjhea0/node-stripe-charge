d4d0888385fa95ebbe9f05adbc6937ce95b2eaad
Roman Valihura <roman.valihura@gmail.com>
2016-10-30 15:26:31 +0200

    routes/api rewrite to es6

--- src/server/routes/api/user.js
+++ src/server/routes/api/user.js
@@ -1,126 +1,132 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose-q')(require('mongoose'));
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose-q')(require('mongoose'));
 
-var passport = require('../../lib/auth');
-var helpers = require('../../lib/helpers');
-var User = require('../../models/user');
+const passport = require('../../lib/auth');
+const helpers = require('../../lib/helpers');
+const User = require('../../models/user');
 
 
 // ** users ** //
 
 // get ALL users
 router.get('/users', helpers.ensureAdminJSON,
-  function(req, res, next) {
-  User.findQ()
-  .then(function(users) {
-    return res.status(200)
-    .json({
-      status: 'success',
-      data: users,
-      message: 'Retrieved users.'
-    });
-  })
-  .catch(function(err) {
-    return next(err);
-  })
-  .done();
-});
+  (req, res, next) => {
+    User.findQ()
+      .then(users => {
+        return res.status(200)
+          .json({
+            status: 'success',
+            data: users,
+            message: 'Retrieved users.'
+          });
+      })
+      .catch(err => {
+        return next(err);
+      })
+      .done();
+  });
 
 // get SINGLE user
 router.get('/users/:id', helpers.ensureAdminJSON,
-  function(req, res, next) {
-  User.findByIdQ(req.params.id)
-  .then(function(user) {
-    res.status(200)
-    .json({
-      status: 'success',
-      data: user,
-      message: 'Retrieved user.'
-    });
-  })
-  .catch(function(err) {
-    return next(err);
-  })
-  .done();
-});
+  (req, res, next) => {
+    User.findByIdQ(req.params.id)
+      .then(user => {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: user,
+            message: 'Retrieved user.'
+          });
+      })
+      .catch(err => {
+        return next(err);
+      })
+      .done();
+  });
 
 // add new user
 router.post('/users', helpers.ensureAdminJSON,
-  function(req, res, next) {
-  User.findOneQ({email: req.body.email})
-    .then(function(existingUser) {
-      if (existingUser) {
-        res.status(409)
-        .json({
-          status: 'error',
-          data: null,
-          message: 'Email is already in use.'
-        });
-      } else {
-        var user = new User(req.body);
-        user.saveQ()
-          .then(function(result) {
-            res.status(200)
+  (req, res, next) => {
+    User.findOneQ({
+      email: req.body.email
+    })
+      .then(existingUser => {
+        if (existingUser) {
+          res.status(409)
             .json({
-              status: 'success',
-              data: {
-                email: user.email,
-                admin: user.admin
-              },
-              message: 'Created user.'
+              status: 'error',
+              data: null,
+              message: 'Email is already in use.'
             });
-          })
-          .catch(function(err) {
-            return next(err);
-          })
-          .done();
-      }
-    })
-    .catch(function(err) {
-      return next(err);
-    })
-    .done();
-});
+        } else {
+          const user = new User(req.body);
+          user.saveQ()
+            .then(result => {
+              res.status(200)
+                .json({
+                  status: 'success',
+                  data: {
+                    email: user.email,
+                    admin: user.admin
+                  },
+                  message: 'Created user.'
+                });
+            })
+            .catch(err => {
+              return next(err);
+            })
+            .done();
+        }
+      })
+      .catch(err => {
+        return next(err);
+      })
+      .done();
+  });
 
 // update SINGLE user
 router.put('/users/:id', helpers.ensureAdminJSON,
-  function(req, res, next) {
-  var id = req.params.id;
-  var update = req.body;
-  var options = {new:true, upsert:true};
-  User.findByIdAndUpdateQ(id, update, options)
-  .then(function(result) {
-    res.status(200)
-    .json({
-      status: 'success',
-      data: result,
-      message: 'Updated user.'
-    });
-  })
-  .catch(function(err) {
-    res.send(err);
-  })
-  .done();
-});
+  (req, res, next) => {
+    const id = req.params.id;
+    const update = req.body;
+    const options = {
+      new: true,
+      upsert: true
+    };
+
+    User.findByIdAndUpdateQ(id, update, options)
+      .then(result => {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: result,
+            message: 'Updated user.'
+          });
+      })
+      .catch(err => {
+        res.send(err);
+      })
+      .done();
+  });
 
 // delete SINGLE user
 router.delete('/users/:id', helpers.ensureAdminJSON,
-  function(req, res, next) {
-  User.findByIdAndRemoveQ(req.params.id)
-  .then(function(user) {
-    res.status(200)
-    .json({
-      status: 'success',
-      data: user,
-      message: 'Removed user.'
-    });
-  })
-  .catch(function(err) {
-    res.send(err);
-  })
-  .done();
-});
+  (req, res, next) => {
+    User.findByIdAndRemoveQ(req.params.id)
+      .then(user => {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: user,
+            message: 'Removed user.'
+          });
+      })
+      .catch(err => {
+        res.send(err);
+      })
+      .done();
+  });
 
 
 module.exports = router;
